bd484ed903f7304b0ed7a73fc6c4cf44693d5f18
Sergey Pozhoga <118672829+Yuvitoly@users.noreply.github.com>
2023-01-10 19:54:08 +0300

    Delete fail1 directory

--- fail1/11.py
+++
@@ -1,8 +0,0 @@
-# Ввести в файл ‘input.txt’ 2 числа в одну строку через пробел.
-# Вывести в файл ‘output.txt’ их разность
-
-with open('input.txt', 'w') as f_input, open('output.txt', 'w') as f_output:
-   a = int(input('Введите целое число: '))
-   b = int(input('Введите целое число: '))
-   f_input.write(f'{a} {b}')
-   f_output.write(f'{a - b}')
